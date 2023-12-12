ff6d980d60278180d17071f428f530fb5337c1c8
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-12-12 23:43:20 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import { MdOutlineSignalWifiStatusbarNull } from "react-icons/md";
+
+function App() {
+
+  return (
+    <>
+      <div className='main-container'>
+        <nav>
+          <div className='hdfc-bank'>
+          <p><b>HDFC BANK</b></p>
+          </div>
+          <div className='debit'>
+            <p> <em> DEBIT</em> </p>
+          </div>
+        </nav>
+            <img className='chips' src="https://www.pngimages.in/uploads/png-webp/2023/May-2023/credit_card_chip_Free_Download_Png.webp" alt="" />
+            <h3 className='millennia'> <code>&gt;</code><tt>MILLENNIA</tt> <code> &lt;</code></h3>
+          <img  className='logo' src="https://clipart-library.com/new_gallery/104-1040692_wifi-png-wi-fi-wifi-symbol-wireless-internet.png" alt="" />
+          <p className='world'>world</p>
+          <div className='circle'><div className='circle-2'></div></div>
+          <div className='atm-no'>  <p >28300016123000000515</p></div>
+      
+          
+          
+      </div>
+    </>
+  )
+}
+
+export default App
